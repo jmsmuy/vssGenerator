49863206a299096345848c5aaaf8dfbf7e331992
jmsmuy <jmsmuy@gmail.com>
2021-02-02 23:15:53 -0300

    changes to fixed duty cycle and freq, adds pot to tps thresh

--- BobbyE30ECUSpeedSensorFooler.ino
+++ BobbyE30ECUSpeedSensorFooler.ino
@@ -1,69 +1,63 @@
-/*
-  Blink
-
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Blink
+/**
+   This is a VSS emulator for some BMW DMEs that need it to allow to rev to the rev limiter and also enables a low idle
+   850hz freq
+   5v on, 0v off, 200mv mean, duty cycle = 2%
 */
 
-// the setup function runs once when you press reset or power the board
-void setup() {
+#define PIN_VSS 1
+#define PIN_TPS 1
+#define PIN_TPS_THRESH 3
+
+#define DUTY_CYCLE 2
+#define PW_FREQ 850
+
+#define TPS_CHECK_CYCLE_COUNT 1000
+
+float const totalDelay = (1000000 / 850);
+float const delayOn = (totalDelay * DUTY_CYCLE) / 100;
+float const delayOff = totalDelay - delayOn;
+int counterForTPSCheck = 0;
+boolean enableVss = false;
+
+void setup() {6
+
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(1, OUTPUT);
-  digitalWrite(1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  pinMode(PIN_VSS, OUTPUT);
+
+  digitalWrite(PIN_VSS, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
-  digitalWrite(1, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(PIN_VSS, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
-  digitalWrite(1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(PIN_VSS, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
-  digitalWrite(1, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(PIN_VSS, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
 }
 
-int counter = 0;
-int newPulsePercentage = 512;
-int newPulseSpeed = 256;
-int delayOn = 1;
-int delayOff = 1;
+int readTPS() {
+  return analogRead(PIN_TPS);
+}
 
-void recalculateDelays() {
-  // Here we calculate in microseconds the delays
-  long totalDelay = (((float)(newPulseSpeed) * (float)14250) / 1024) + (float)750;
-  int delayPercentageOn = ((float)(newPulsePercentage) * (float)100) / (float)1023;
-  delayOn = (totalDelay * delayPercentageOn) / 100;
-  delayOff = (totalDelay * (100 - delayPercentageOn)) / 100;
+int readTPSThresh() {
+  return analogRead(PIN_TPS_THRESH);
+}
+
+boolean shouldEnableVssOutput() {
+  int tpsValue = readTPS();
+  int tpsThresh = readTPSThresh();
+  return tpsValue > tpsThresh;
 }
 
 void loop() {
-  if (counter > 300) {
-    newPulsePercentage = analogRead(3);
-    counter = 0;
-  } else if (counter > 150) {
-    newPulseSpeed = analogRead(2);
+  if (enableVss) {
+    digitalWrite(PIN_VSS, HIGH);
+    delayMicroseconds(delayOn);
+    digitalWrite(PIN_VSS, LOW);
+    delayMicroseconds(delayOff);
   }
-  digitalWrite(1, HIGH);
-  delayMicroseconds(delayOn);
-  digitalWrite(1, LOW);
-  delayMicroseconds(delayOff);
-
-  if (counter == 0) {
-    recalculateDelays();
+  counterForTPSCheck++;
+  if (counterForTPSCheck > TPS_CHECK_CYCLE_COUNT) {
+    counterForTPSCheck = 0;
+    enableVss = shouldEnableVssOutput();
   }
-  counter++;
 }
